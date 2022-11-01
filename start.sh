2ace5f14c8dabfbe9aa051dbb388a52dadbe6f67
Vinicius Marteleto <vinicius.silveira@irroba.com.br>
2022-10-31 23:15:26 -0300

    configs

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+cd user-laravel-service
+
+docker-compose down
+
+docker-compose up -d --build
+
+docker-compose exec laravel /bin/bash -c "composer install && chmod -R 777 storage/ && php artisan key:generate"
+
+cd ..
+
 cd company-nestjs-service
 
 docker-compose down
@@ -10,14 +20,9 @@ cd ..
 
 cd user-laravel-service
 
-docker-compose down
+docker-compose exec laravel /bin/bash -c "php artisan migrate:fresh"
 
-docker-compose up -d --build
-
-docker-compose exec laravel /bin/bash -c "composer install && chmod -R 777 storage/ && php artisan key:generate"
-
-# cd user-laravel-service && docker-compose exec laravel /bin/bash -c "php artisan migrate:fresh" && cd ..
+cd ..
 
 # cd user-laravel-service && docker-compose exec laravel /bin/bash -c "php artisan queue:work sqs --queue=users"
 
-cd ..
