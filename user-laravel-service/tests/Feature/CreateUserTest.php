d0febdf133bb3dfa326e3e0d770a695290da548f
Vinicius Marteleto <vinicius.silveira@irroba.com.br>
2022-11-01 02:01:42 -0300

    testing

--- user-laravel-service/tests/Feature/CreateUserTest.php
+++ user-laravel-service/tests/Feature/CreateUserTest.php
@@ -18,14 +18,29 @@ class CreateUserTest extends TestCase
     {
         $user = User::factory()->definition();
         $response = $this->post('/api/users', $user, ['Accept' => 'application/json']);
-        $response->assertStatus(201);
+        $response->assertCreated();
     }
 
     public function test_create_user_with_invalid_company_cnpj()
     {
         $user = User::factory()->definition();
-        $user['company_cnpj'] = '123';
+        $user['company_cnpj'] = 'invalid cnpj';
         $response = $this->post('/api/users', $user, ['Accept' => 'application/json']);
         $response->assertStatus(400);
+    }
+
+    public function test_create_user_with_email_already_taken()
+    {
+        $user = User::factory()->create();
+        $response = $this->post('/api/users', $user->toArray(), ['Accept' => 'application/json']);
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => 'Invalid request',
+            'data' => [
+                'email' => [
+                    'The email has already been taken.'
+                ]
+            ]
+        ]);
     }
 }
